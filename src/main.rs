29ae6d6eb3299b2a0621c1522cf2da15fb5d42f6
Mordecai Malignatus <az4reus@gmail.com>
2018-10-03 07:52:23 +0200

    Feat(main): Conditional inclusion of win-clipboard/clipboard2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate notify;
+
+#[cfg(windows)]
+extern crate clipboard_win;
+#[cfg(unix)]
+extern crate clipboard2;
+
+mod clipboard_poller;
+mod types;
+
 fn main() {
     println!("Hello, world!");
 }
