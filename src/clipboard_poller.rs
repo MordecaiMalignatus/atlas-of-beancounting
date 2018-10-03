13228aefaa99b5eb0da0d6a60441d5a9bdaad5e5
Mordecai Malignatus <az4reus@gmail.com>
2018-10-03 08:16:32 +0200

    Feat(clipboard): Introduce first sketch at a clipboard poller.

--- src/clipboard_poller.rs
+++ src/clipboard_poller.rs
@@ -1,10 +1,34 @@
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender};
 use std::thread::sleep;
+use std::time::Duration;
 use types::clipboard_event::ClipboardEvent;
+#[cfg(windows)]
+use clipboard_win::Clipboard;
+#[cfg(unix)]
+use clipboard2;
 
+/// Poll clipboard every 200ms, trying to grab everything that happens. Might be
+/// not good performance wise.
+#[cfg(windows)]
 pub fn watch_clipboard(s: Sender<ClipboardEvent>) -> ! {
+    let mut current_content: String;
     loop {
+        // TODO: Miiiiiight need better error handling. Or any at all.
+        let contents = Clipboard::new().unwrap().get_string().unwrap();
+        match contents {
+            current_content => {},
+            _ => {
+                current_content = contents;
+                s.send(ClipboardEvent{content: current_content});
+            }
+        }
 
+        sleep(Duration::from_millis(200));
     }
+}
+
+#[cfg(unix)]
+pub fn watch_clipboard(s: Sender<ClipboardEvent>) -> ! {
+    panic!("Unix not yet implemented, send a patch!");
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ extern crate clipboard2;
 mod clipboard_poller;
 mod types;
 
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
     println!("Hello, world!");
+
+    let (sender, receiver) = mpsc::channel();
+
+    thread::spawn(move || {
+        clipboard_poller::watch_clipboard(sender);
+    });
+
+    println!("{:?}", receiver.recv());
 }
