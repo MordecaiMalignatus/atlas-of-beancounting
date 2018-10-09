ea741a6c7a5cd2a4fc84bad028369bbbbcab4ad6
Mordecai Malignatus <az4reus@gmail.com>
2018-10-09 05:52:42 +0200

    Feat(log_watcher): Update core loop to retrieve last line.

--- src/log_watcher.rs
+++ src/log_watcher.rs
@@ -1,27 +1,42 @@
-use std::sync::mpsc::channel;
-use std::sync::mpsc::{Sender};
-use std::time::Duration;
-use std::path::PathBuf;
-use std::io::ErrorKind;
+use notify::{raw_watcher, RawEvent, RecursiveMode, Watcher};
+use std::fs::File;
 use std::io::Error;
+use std::io::ErrorKind;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::path::Path;
+use std::path::PathBuf;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Sender;
 use types::zone_event::ZoneEvent;
-use notify::{Watcher, raw_watcher, RecursiveMode, RawEvent};
 
 pub fn watch_zone_log(s: Sender<ZoneEvent>) -> ! {
-    let  (watcher_sender, watcher_receiver) = channel();
+    let (watcher_sender, watcher_receiver) = channel();
     let mut watcher = raw_watcher(watcher_sender).unwrap();
+    let mut log_offset = 0 as u64;
     let filepath = guess_event_path().expect("Can't start watcher without event log.");
 
-    watcher.watch(filepath, RecursiveMode::NonRecursive).unwrap();
+    watcher
+        .watch(filepath.clone(), RecursiveMode::NonRecursive)
+        .unwrap();
 
     loop {
         match watcher_receiver.recv() {
-            Ok(RawEvent{path: Some(path), op: Ok(op), cookie}) =>
-                println!("{:?}, {:?}, {:?}: Raw event received", path, op, cookie),
-            Ok(event) =>
-                println!("Got broken event: {:?}", event),
-            Err(e) =>
-                panic!("Caught error {:?}", e),
+            Ok(RawEvent {
+                path: Some(path),
+                op: Ok(op),
+                cookie,
+            }) => {
+                println!("{:?}, {:?}, {:?}: Raw event received", path, op, cookie);
+                let (last_line, new_offset) = get_last_line_of_log(filepath.as_path(), log_offset)
+                    .expect("Couldn't read from file");
+                log_offset = new_offset;
+
+                println!("New last line from log: {}", last_line);
+            }
+            Ok(event) => println!("Got broken event: {:?}", event),
+            Err(e) => println!("Caught error {:?}", e),
         }
     }
 }
