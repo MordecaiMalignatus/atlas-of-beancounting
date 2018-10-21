921df838a3ef30ad15b3c43a982b69d0213df0f0
Mordecai Malignatus <az4reus@gmail.com>
2018-10-21 13:45:23 +0200

    Feat(types): add ItemRarity Enum, refactor Requirements.

--- src/types/item.rs
+++ src/types/item.rs
@@ -3,15 +3,26 @@
 pub struct Item {
     pub description: String,
     pub name: String,
+    pub rarity: ItemRarity,
     pub affixes: Vec<String>,
     pub sockets: Option<String>,
     pub item_level: u32,
-    pub attribute_requirements: Option<AttributeRequirements>,
+    pub requirements: Option<Requirements>,
 }
 
 #[derive(Debug)]
-pub struct AttributeRequirements {
+pub struct Requirements {
+    pub level: u32,
     pub strength: u32,
     pub intelligence: u32,
     pub dexterity: u32,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ItemRarity {
+    Currency,
+    Normal,
+    Magical,
+    Rare,
+    Unique,
 }
