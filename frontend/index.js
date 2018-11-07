7dc6738f31bf12d20d3f91cda155f3b02638fc40
Mordecai Malignatus <az4reus@gmail.com>
2018-11-07 06:58:53 +0100

    Feat(Frontend): Add some basic CSS.
    This is more like playing around, really.

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const Item = (props) => <div className="measure-narrow"> {props.name} </div>;
+const append = (oldArray, newItem) => [...oldArray, newItem];
+
+const Item = (props) => (
+	<div className="measure-narrow fl-20 bg-light-gray shadow-3 ma2 pa3 b--mid-grey">
+	{props.name}
+        </div>
+);
 
 class EventPipe extends React.Component {
     constructor(props) {
@@ -13,14 +19,14 @@ class EventPipe extends React.Component {
     addElement() {
 	const item = <Item name="Chaos Orb" />;
 	this.setState({
-	    items: [...this.state.items, item]
+	    items: append(this.state.items, item)
 	});
     }
 
     render() {
-	return <div className="fl w-25 r">
-	    <h1>Event Pipe :D</h1>
-	    <button onClick={this.addElement}> Add an element!</button>
+	return <div className="helvetica fl w-20 br ma2 pa1 bg-moon-gray">
+	    <h1 className="h1">Event Pipe</h1>
+	    <button className="br2 pa2"onClick={this.addElement}> Add an element!</button>
 	    <div>There are {this.state.items.length} items in the pipe.</div>
 	    <div>{this.state.items}</div>
 	</div>;
