8aa0e0ac82f2a2800a46ffa9346b34cb4843fc64
Mordecai Malignatus <az4reus@gmail.com>
2018-11-07 06:41:54 +0100

    Feat(frontend): Implement first draft of event pipe.

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const mountNode = document.getElementById("mount");
+const Item = (props) => <div className="measure-narrow"> {props.name} </div>;
 
-class Item extends React.Component {
+class EventPipe extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state  = {items: []};
+	this.addElement = this.addElement.bind(this);
+    }
+
+    addElement() {
+	const item = <Item name="Chaos Orb" />;
+	this.setState({
+	    items: [...this.state.items, item]
+	});
+    }
+
     render() {
-	return <div id="test">
-	    Hello {this.props.name}!
+	return <div className="fl w-25 r">
+	    <h1>Event Pipe :D</h1>
+	    <button onClick={this.addElement}> Add an element!</button>
+	    <div>There are {this.state.items.length} items in the pipe.</div>
+	    <div>{this.state.items}</div>
 	</div>;
     }
 }
 
-ReactDom.render(
-	<Item name="Meee"/>,
-        mountNode);
+const mountNode = document.getElementById("mount");
+ReactDom.render(<EventPipe />, mountNode);
