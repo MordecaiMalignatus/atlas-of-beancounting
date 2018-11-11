ad8a24e4cbb15adc73e31e630e32c3addf00a8b9
Mordecai Malignatus <az4reus@gmail.com>
2018-11-11 09:31:55 +0100

    Feat(Frontend): Refactor EventPipe to function.
    The state is now kept centrally in the Display component.

--- frontend/index.js
+++ frontend/index.js
@@ -10,35 +10,14 @@ const Item = props => (
   </div>
 );
 
-class EventPipe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [] };
-    this.addElement = this.addElement.bind(this);
-  }
-
-  addElement() {
-    const item = <Item name="Chaos Orb" value="100c" />;
-    this.setState({
-      items: append(this.state.items, item)
-    });
-  }
-
-  render() {
-    return (
-      <div className="fl w-20 ma2 pa1">
-        <h1 className="h1">Event Pipe</h1>
-        <button className="br2 pa2" onClick={this.addElement}>
-          Add an element!
-        </button>
-        <div className="pt2">
-          There are {this.state.items.length} items in the pipe.
-        </div>
-        <div className="pt3 item-center">{this.state.items}</div>
-      </div>
-    );
-  }
-}
+const EventPipe = props => {
+  return (
+    <div className="fl w-20 ma2 pa1">
+      <h1 className="h1">Item Log</h1>
+      <div className="pt3 item-center">{props.items}</div>
+    </div>
+  );
+};
 
 const MapDisplay = props => {
   return (
@@ -58,17 +37,21 @@ const MapDisplay = props => {
 class Display extends React.Component {
   constructor(props) {
     super(props);
+
     this.dispatch = this.dispatch.bind(this);
     window.dispatch = this.dispatch;
+
     this.state = {
-      currentMap: "Shaped Cage"
+      currentMap: "Shaped Cage",
+      previousMap: "",
+      droppedItems: []
     };
   }
 
   render() {
     return (
       <div className="fl w-100 helvetica">
-        <EventPipe />
+        <EventPipe items={this.state.droppedItems} />
         <MapDisplay
           currentMap={this.state.currentMap}
           previousMap={this.state.previousMap}
@@ -80,7 +63,8 @@ class Display extends React.Component {
   dispatch(string) {
     this.setState({
       currentMap: string,
-      previousMap: this.state.currentMap
+      previousMap: this.state.currentMap,
+      droppedItems: append(this.state.droppedItems, <Item name={string} />)
     });
   }
 }
