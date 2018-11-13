f05cede397888baab2a8efa36e4590915fc5ff41
Mordecai Malignatus <az4reus@gmail.com>
2018-11-13 06:19:13 +0100

    Feat(web_client): Currency-overview query stub
    This requires a solution for the deserialization, and will probably require
    further refactoring to use a central client, and just hand off requests and
    responses to other functions.

--- src/web_client.rs
+++ src/web_client.rs
@@ -1,18 +1,34 @@
-use std::io::{self, Write};
+use chrono::prelude::*;
+use reqwest::{Client, Error, Method, Request, Url};
+
+use constants::CURRENT_LEAGUE;
 use types::item::Item;
 use types::pricing::Price;
-use reqwest::Error;
-use reqwest::get;
 
-// pub fn get_price() -> () {
-//     rt::run(get_request());
-// }
+fn get_currency_prices(client: &Client) -> Result<Vec<Price>, Error> {
+    let today = Local::now().format("%Y-%m-%d").to_string();
+    let league = CURRENT_LEAGUE.clone();
 
-pub fn get_request() -> Result<(), Error> {
-    let url =
-        "http://poe.ninja/api/data/currencyoverview?league=Delve&type=Currency&date=2018-11-12";
+    let endpoint = Url::parse(&format!(
+        "https://poe.ninja/api/data/currencyoverview?league={}&type=Currency&date={}",
+        today, league
+    )).unwrap();
 
-    let mut resp = get(url)?;
-    println!("Body: \n\n{}", resp.text()?);
-    Ok(())
+    let request = Request::new(Method::GET, endpoint);
+    let mut response = client.execute(request)?;
+
+    match response.status().is_success() {
+        true => {
+            // This won't actually work. Possible options here are either:
+            // 1. Stubbing out the entire return struct from poe.ninja and
+            // marking a lot of fields with #[skip_deserialize] to avoid a
+            // shitton of data I have no need for.
+
+            // 2. Using `serde_json::Value' as the main type in a Map and
+            // operating on it like a hasahmap.
+            let body: Vec<Price> = response.json()?;
+            Ok(body)
+        }
+        false => unimplemented!(),
+    }
 }
