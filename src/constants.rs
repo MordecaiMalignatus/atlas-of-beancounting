ba02784afbef876ad5dca65e29a1154d69117f76
Mordecai Malignatus <az4reus@gmail.com>
2018-11-26 06:46:30 +0100

    Feat(web_client): Implement batch cache refresh.
    This removes the previous draft for updating the cache, as well as the comment
    laying out considerations.
    
    It simply updates everything at once, instead of futzing about with
    endpoint-specific expiration.
    
    Future considerations:
    - The expiration date is now global, there is no need to duplicate it for every
    entry
    - A lot of information is lost when converting to `Price`, there could be more
    information kept depending on uses. For now this will do.

--- src/constants.rs
+++ src/constants.rs
@@ -2,10 +2,10 @@
 /// https://www.pathofexile.com/developer/docs/api-resource-leagues
 pub const CURRENT_LEAGUE: &str = "Delve";
 
-/// URLs for poe.ninja. These require to be `format!`ed with the league name
-/// before use. These are overspecific because the API is locked down pretty
-/// tightly -- These are the endpoints that work and I haven't gotten any others
-/// to work.
+/// URLs for poe.ninja. These need to have the `{}` `replace`d with the league
+/// name before use.  before use. These are overspecific because the API is
+/// locked down pretty tightly -- These are the endpoints that work and I
+/// haven't gotten any others to work.
 pub const POE_NINJA_ENDPOINT_TEMPLATES: &[&str] = &[
     "https://poe.ninja/api/data/currencyoverview?league={}&type=Currency",
     "https://poe.ninja/api/data/currencyoverview?league={}&type=Fragment",
--- src/web_client.rs
+++ src/web_client.rs
@@ -5,7 +5,7 @@ use reqwest::{Client, Error, Method, Request, Url};
 use std::collections::HashMap;
 use std::sync::mpsc::{Receiver, Sender};
 
-use constants::CURRENT_LEAGUE;
+use constants::{CURRENT_LEAGUE, POE_NINJA_ENDPOINT_TEMPLATES};
 use types::poe_ninja::NinjaCurrencyOverviewResponse;
 use types::pricing::{Price, PriceMessage};
 
@@ -14,8 +14,6 @@ type PriceCache = HashMap<String, CacheEntry>;
 struct CacheEntry {
     price: Price,
     expiration: DateTime<Local>,
-    // This is the "kind" of an item according to poe.ninja, like "Currency" or "Fragment".
-    source_category: String,
 }
 
 fn spawn_price_bot(recv: Receiver<PriceMessage>, sender: Sender<PriceMessage>) -> ! {
@@ -50,74 +48,37 @@ fn query_cache(_cache: &mut PriceCache, _key: &str) -> Option<Price> {
     unimplemented!()
 }
 
-/*
-Cache update can be implemented in one function, since the "source_category" is
-contained in the cahce. This turns it into a recursive function:
-
-- iterate through entries, find the first expired
-- Update the category belonging to that item, yield updated cache, recurse
-- If nothing is expired at some point, you can just return.
-
-Concerns about this:
-
-- This would be a sequential update for something that is parallalizeable, and
-  easily so. I could make a list for all the URLs, run out all the requests,
-  turn it into `CacheEntry` structs, and then sequentially insert into the
-  cache.
-- Everything expires at roughly the same time anyway (due to on-start
-  information grabbing) making a bulk-update function very convenient.
-- A scheduled bulk update could be done on a separate thread and then just sent
-  to the pricing actor, obviating the need to do it here, and just slotting the
-  new cache into place.
-*/
-
-fn refresh_gear_cache(_cache: &mut PriceCache) -> Result<(), Error> {
-    Ok(())
-}
-
-fn refresh_currency_cache(cache: &mut PriceCache) -> Result<(), Error> {
-    // TODO: Find some way to not construct the client per request and pass it
-    // here somehow.
-    let raw_prices = get_currency_prices(&Client::new())?;
-    let prices: Vec<Price> = raw_prices
-        .lines
-        .into_iter()
-        .map(|line| line.into())
+/// Refresh the passed cache with new poe.ninja data.  This will hit up all the
+/// endpoints in poe.ninja and move all the response data into the cache --
+/// currently sequentially. This would benefit greatly from throwing a rayon
+/// par_iter() on there, even though I'm not sure how well Reqwest handles
+/// that. Definitely needs to be made either asynchronous or parallel.
+fn refresh_gear_cache(cache: &mut PriceCache) -> Result<(), Error> {
+    let client = Client::new();
+    let prices: Vec<Price> = POE_NINJA_ENDPOINT_TEMPLATES
+        .to_vec()
+        .iter()
+        .map(|url| url.replace("{}", CURRENT_LEAGUE))
+        .map(|url| client.get(&url).send())
+        .filter(|result| result.is_ok())
+        .map(|res| res.expect("Can't unwrap Request when refreshing gear cache"))
+        .filter(|resp| resp.status().is_success())
+        .map(|mut resp| {
+            resp.json::<NinjaCurrencyOverviewResponse>()
+                .expect("Should be able to parse a poe.ninja response.")
+        }).flat_map(|resp| resp.lines.into_iter().map(|line| Price::from(line)))
         .collect();
+
+    println!("Fetched {} prices, updating cache...", prices.len());
 
     let _ = prices
         .into_iter()
         .map(|price| CacheEntry {
             price,
-            source_category: "currency".to_string(),
             expiration: calculate_expiration_date(Local::now()),
-        }).map(|cache_entry| cache.insert(cache_entry.price.currency_name.clone(), cache_entry));
+        }).map(|entry| cache.insert(entry.price.currency_name.clone(), entry));
 
     Ok(())
-}
-
-fn get_currency_prices(client: &Client) -> Result<NinjaCurrencyOverviewResponse, Error> {
-    let url = create_request_url("currencyoverview", "Currency");
-    let request = Request::new(Method::GET, url);
-    let mut response = client.execute(request)?;
-
-    match response.status().is_success() {
-        true => {
-            let body: NinjaCurrencyOverviewResponse = response.json()?;
-            Ok(body)
-        }
-        false => unimplemented!(),
-    }
-}
-
-fn create_request_url(endpoint: &str, kind: &str) -> Url {
-    let today = Local::now().format("%Y-%m-%d").to_string();
-    let string = format!(
-        "https://poe.ninja/api/data/{}?league={}&type={}&date={}",
-        endpoint, CURRENT_LEAGUE, kind, today
-    );
-
-    Url::parse(&string).unwrap()
 }
 
 fn calculate_expiration_date(now: DateTime<Local>) -> DateTime<Local> {
@@ -130,32 +91,5 @@ fn calculate_expiration_date(now: DateTime<Local>) -> DateTime<Local> {
 mod test {
     use super::*;
 
-    mod cache_update {
-        use super::*;
-
-        #[test]
-        fn should_update_currency_cache() {
-            let mut cache = PriceCache::new();
-            refresh_currency_cache(&mut cache);
-            // T'is a magic number test that's valid as of Delve, it's the
-            // number of items returned by the poe.ninja api when queried for
-            // the currency overview.
-            assert_eq!(cache.len(), 45);
-        }
-    }
-
-    #[test]
-    fn should_deserialize_correctly() {
-        // This can be considered successful if serde does not fail while trying
-        // to deserialize. It's also a good way to watch for API changes.
-        match get_currency_prices(&Client::new()) {
-            Ok(body) => {
-                println!("{:?}", body);
-            }
-            Err(e) => {
-                println!("error happened: {}", e);
-                assert!(false);
-            }
-        }
-    }
+    // Needs new tests, I obsoleted them all :(
 }
